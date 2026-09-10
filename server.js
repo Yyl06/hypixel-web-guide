750d0f301c671704070958999a0f07346e992825
Yyl <yuanlinplays@gmail.com>
2026-09-10 21:16:08 +0800

    Redesigned the whole backend structure for better future maintainence

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.render('index', { pageTitle: 'Game Guide' });
 });
 
 app.get('/farming-guide/pdf', (req, res) => {
@@ -18,7 +20,7 @@ const pageRoutes = {
 
 Object.entries(pageRoutes).forEach(([route, folder]) => {
   app.get(route, (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', folder, 'index.html'));
+    res.render(folder, { pageTitle: folder === 'farming-guide' ? 'Farming Guide | Koebatz Guide' : folder });
   });
 });
 
