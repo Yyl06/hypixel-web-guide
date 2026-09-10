06e347ddee3121c58f678aea4f1b78f8f71fc54a
Yyl <yuanlinplays@gmail.com>
2026-09-10 20:57:26 +0800

    Added Farming Guides

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  res.send('Hello from Express!');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('/about', (req, res) => {
-  res.send('About page');
+app.get('/farming-guide/pdf', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Farming.pdf'));
+});
+
+const pageRoutes = {
+  '/guides': 'farming-guide',
+};
+
+Object.entries(pageRoutes).forEach(([route, folder]) => {
+  app.get(route, (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', folder, 'index.html'));
+  });
 });
 
 // Vercel imports the app as a serverless function; listen only for local runs.
